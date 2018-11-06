fcf1f50ff67d8d26d4b404ae24c9abc6ca05b446
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-11-06 16:44:02 -0500

    coffee shop initial commit

---
+++ coffeeshop/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your registration page.";
+
+            return View();
+        }
+        public ActionResult AddUser(UserInput newUser)
+        {
+            // ToDo: validation!!!!!!
+
+            if (ModelState.IsValid)
+            {
+                // ToDo: Send the data to the DB
+
+                // confirmation, or maybe send to the Index page
+
+                //ViewData["ConfMessage"] = "Thanks " + newUser.FirstName;
+
+                ViewBag.ConfMessage = "Thanks " + newUser.FirstName;
+
+                return View("Confirm");
+            }
+
+            else
+            {
+                return View("Error");
+            }
+        }
+
+    }
+}
---
+++ coffeeshop/Models/UserInput.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class UserInput
+    {
+        [Required] // attributes 
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string FirstName { set; get; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string LastName { set; get; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string Email { set; get; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string Password { set; get; }
+
+        [Required]
+        [RegularExpression("^[0-9]{10,}$")]//change this
+        public string PhoneNumber { set; get; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string BirthDate { set; get; }
+
+        public UserInput()
+        {
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            PhoneNumber = 0;
+            Password = "";
+        }
+        public UserInput(string fn, string ln, string em, int pn, string pw)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Email = em;
+            PhoneNumber = pn;
+            Password = pw;
+        }
+
+    }
+}
